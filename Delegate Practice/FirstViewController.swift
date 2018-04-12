0105ed2d4d9032dbf15203f3d14dcc3557b387cb
Pavan Sabnis <pvnsbns@gmail.com>
2018-04-11 17:45:36 -0700

    gave segue an identifier, created the two view controllers.

---
+++ Delegate Practice/FirstViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FirstViewController.swift
+//  Delegate Practice
+//
+//  Created by Pavan Sabnis on 4/11/18.
+//  Copyright © 2018 Pavan Sabnis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
---
+++ Delegate Practice/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Delegate Practice
+//
+//  Created by Pavan Sabnis on 4/11/18.
+//  Copyright © 2018 Pavan Sabnis. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
+
